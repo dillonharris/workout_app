4e8ac61c898ed07771b42cb62effb0990b99e864
dillon harris <harrisdillon0@gmail.com>
2016-03-09 22:47:25 +0200

    Create exercises successfully

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'dashboard/index'
 
-  root 'dashboard#index'
+  resources :users do 
+  	resources :exercises
+  end
+
+  root to: 'dashboard#index'
+
 end
